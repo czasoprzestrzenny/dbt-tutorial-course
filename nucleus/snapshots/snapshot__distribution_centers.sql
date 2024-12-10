dc4f8083f4c0533c91e5900fcb2bbc64b88db846
STARYJ01 <jaroslaw.stary@kingfisher.com>
2024-12-10 16:25:57 +0100

    added snapshot

---
+++ nucleus/snapshots/snapshot__distribution_centers.sql
@@ -0,0 +1,22 @@
+{% snapshot snapshot__distribution_centers %}
+
+{{
+	config(
+		target_schema='dbt_snapshots',
+		unique_key='id',
+		strategy='timestamp',
+		updated_at='updated_at'
+	)
+}}
+
+SELECT
+	id,
+	name,
+	latitude,
+	longitude,
+	CURRENT_TIMESTAMP() as updated_at
+
+FROM {{ source('thelook_ecommerce', 'distribution_centers') }}
+WHERE id < 3
+
+{% endsnapshot %}
