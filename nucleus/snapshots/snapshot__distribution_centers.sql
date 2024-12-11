88722b82bcc22bc6a1d5a04a824a9484ee7c4cd4
STARYJ01 <jaroslaw.stary@kingfisher.com>
2024-12-11 13:13:54 +0100

    changes

--- nucleus/snapshots/snapshot__distribution_centers.sql
+++ nucleus/snapshots/snapshot__distribution_centers.sql
@@ -20,3 +20,19 @@ FROM {{ source('thelook_ecommerce', 'distribution_centers') }}
 WHERE id < 3
 
 {% endsnapshot %}
+
+
+{# {% snapshot snapshot__distribution_centers %}
+
+{{
+    config(
+      target_schema='dbt_snapshots',
+      unique_key='id',
+      strategy='check',
+      check_cols=['name', 'latitude', 'longitude']
+    )
+}}
+
+SELECT * FROM {{ source('thelook_ecommerce', 'distribution_centers') }}
+
+{% endsnapshot %} #}
